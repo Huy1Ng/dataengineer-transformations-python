e96b149991b402dde962157bf4cb917b4bd0fc4f
Huy Nguyen <huy.nguyen7994@gmail.com>
2025-09-19 07:06:08 +0700

    add alternative path to solution

--- data_transformations/citibike/distance_transformer.py
+++ data_transformations/citibike/distance_transformer.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession, DataFrame
 import pyspark.sql.functions as F
+from pyspark.sql.types import DoubleType
 
 METERS_PER_FOOT = 0.3048
 FEET_PER_MILE = 5280
@@ -22,6 +23,24 @@ def calculate_haversine_distance(
     c = 2 * F.atan2(F.sqrt(a), F.sqrt(1 - a))
 
     return F.round(earth_radius * c, 3)
+
+
+@F.udf(returnType=DoubleType())
+def udf_calculate_haversine_distance(lat1, lon1, lat2, lon2):
+    from math import radians, sin, cos, atan2, sqrt
+
+    earth_radius = 3963  # in miles
+    theta_1 = radians(lat1)
+    theta_2 = radians(lat2)
+    delta_theta = radians(lat2 - lat1)
+    delta_lambda = radians(lon2 - lon1)
+
+    a = (sin(delta_theta / 2) ** 2) + cos(theta_1) * cos(theta_2) * (
+        sin(delta_lambda / 2) ** 2
+    )
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    return round(earth_radius * c, 3)
 
 
 def compute_distance(
@@ -48,6 +67,7 @@ def run(
     dataset_with_distances = compute_distance(
         spark, input_dataset, calculate_haversine_distance
     )
+    dataset_with_distances.explain()
     dataset_with_distances.show()
 
     dataset_with_distances.write.parquet(transformed_dataset_path, mode="append")
--- data_transformations/wordcount/word_count_transformer.py
+++ data_transformations/wordcount/word_count_transformer.py
@@ -7,6 +7,12 @@ import pyspark.sql.functions as F
 def run(spark: SparkSession, input_path: str, output_path: str) -> None:
     logging.info("Reading text file from: %s", input_path)
     input_df = spark.read.text(input_path)
+    # SQL
+    # input_df.createOrReplaceTempView("kafka")
+    # out_df = spark.sql(
+    #     r"""with explode_text as (select explode(regexp_extract_all(lower(value), r"[\w'']+", 0)) as word from kafka) select word, count(*) as count from explode_text group by word order by word asc"""
+    # )
+    # Dataframe
     out_df = (
         input_df.withColumn(
             "word",
@@ -16,6 +22,7 @@ def run(spark: SparkSession, input_path: str, output_path: str) -> None:
         .count()
         .sort("word", ascending=True)
     )
+    out_df.explain()
     out_df.show()
     logging.info("Writing csv to directory: %s", output_path)
 
